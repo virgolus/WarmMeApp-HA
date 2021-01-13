e88ea0360f2e522274cbf935d433788b2ca32f28
Enrico Vergine <virgolusjr@gmail.com>
2021-01-14 00:24:14 +0100

    aggiunta configurazione e test gpio

---
+++ scripts/prova.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+
+while (True):
+    GPIO.output(20, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(21, GPIO.LOW)
+    time.sleep(1)
+    GPIO.output(20, GPIO.LOW)
+    time.sleep(0.5)
+    GPIO.output(21, GPIO.HIGH)
