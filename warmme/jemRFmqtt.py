23f2df5dbc1ae5c337d291a9711a0a453d2b7a65
Ezio Vergine <virgolus@gmail.com>
2021-02-17 21:39:51 +0100

    pulizia

--- warmme/jemRFmqtt.py
+++ warmme/jemRFmqtt.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import sys
 from threading import Thread
-#from bme280 import process_bme_reading
 from rflib import rf2serial, fetch_messages, request_reply
 import rflib
 from time import sleep
@@ -14,6 +13,9 @@ import logging
 from random import randint
 import threading
 
+timeOut=3600			#timeout per sensore offline
+timerSensorCheck=3600		#intervallo check stato sensori
+
 config = configparser.ConfigParser()
 config.read('/home/pi/warmme.properties')
 logging.basicConfig(filename='/home/pi/WarmMeApp-HA/logs/Sensors.log', filemode='w',format='%(asctime)s - %(message)s', level=logging.DEBUG)
@@ -21,8 +23,8 @@ broker=config['mqtt']['url']
 
 class sensor:
     def __init__(self,id,HAid):
-        self.id=id
-        self.HAid=HAid
+        self.id=id				#ID sensore
+        self.HAid=HAid				#ID sensore  assegnato su home assistant "home-assistant/sensor/HAid"
         self.temp=""
         self.hum=""
         self.battery=""
@@ -40,12 +42,13 @@ class sensor:
       self.lastReading=datetime.now()
       return msgReady
 
+#elenco sensori:
 ListofSensors=[sensor('94','1'), #sala
                sensor('95','2'), #notte
                sensor('96','3'), #bagno
                sensor('93','4')] #cucina
 
-def findSensor(List,id):    
+def findSensor(List,id):
     return next((x for x in List if x.id==id),None)
 
 def timer(myListofSensors):
@@ -55,13 +58,13 @@ def timer(myListofSensors):
 def checkSensorstatus(ListofSensors):
     for asensor in ListofSensors:
         timeNow=datetime.now()
-        timediff=timeNow-asensor.lastReading    
+        timediff=timeNow-asensor.lastReading
         logging.debug("sensorID: " + asensor.HAid + " diff from last reading: "+ str(timediff))
         if timediff.seconds >600:
           status_topic=config['mqtt']['sensor_topic']+"/"+asensor.HAid+"/status"
           client= mqtt.Client("client_status"+str(randint(0, 100)))
           client.on_publish=on_publish
-          client.connect(broker) #connect
+          client.connect(broker)
           client.publish(status_topic,payload="offline")
 
 def on_publish(client,userdata,result):
@@ -89,7 +92,7 @@ def inbound_message_processing():
                 topic=config['mqtt']['sensor_topic']+"/"+Mysensor.HAid
                 client= mqtt.Client("pi_rf_"+str(randint(0, 100)))
                 client.on_publish=on_publish
-                client.connect(broker) #connect
+                client.connect(broker)
                 client.publish(topic,payload=json.dumps(jsonMessage))
                 status_topic=config['mqtt']['sensor_topic']+"/"+Mysensor.HAid+"/status"
                 client.publish(status_topic,payload="online")
@@ -98,18 +101,14 @@ def inbound_message_processing():
 
         if rflib.event.is_set():
             break
-  except Exception as e: 
+  except Exception as e:
         template = "An exception of type {0} occurred. Arguments:\n{1!r}"
         message = template.format(type(e).__name__, e.args)
         logging.error(e)
         rflib.event.set()
         exit()
 
-def main(): 
-  # client= mqtt.Client("pi_rf_"+str(randint(0, 100)))
-  # client.on_publish=on_publish
-  # client.connect(broker) #connect
-
+def main():
   rflib.init()
 
   #start serial processing thread
@@ -119,7 +118,7 @@ def main():
   #now start processing thread
   b=Thread(target=inbound_message_processing, args=())
   b.start()
-  
+
   timer(ListofSensors)
 
   while not rflib.event.is_set():
